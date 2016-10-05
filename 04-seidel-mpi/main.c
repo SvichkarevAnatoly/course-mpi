31c19345b51c63d1ffee3f552feb07a7b113e4aa
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-10-05 13:05:20 +0300

    try using scatter

--- 04-seidel-mpi/main.c
+++ 04-seidel-mpi/main.c
@@ -1,13 +1,43 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <malloc.h>
+
+void generate_data(int *buf, int size) {
+    int i;
+    for (i = 0; i < size; ++i) {
+        buf[i] = i;
+    }
+}
+
+void print_data(int buf[], const int size, int rank) {
+    int i;
+    for (i = 0; i < size; ++i) {
+        printf("r%d %d\n", rank, buf[i]);
+    }
+    printf("\n");
+}
 
 int main(int argc, char *argv[]) {
     int rank, size;
 
-    MPI_Init(&argc, &argv);      /* starts MPI */
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);        /* get current process id */
-    MPI_Comm_size(MPI_COMM_WORLD, &size);        /* get number of processes */
-    printf("Hello world %d of %d\n", rank, size);
+    const int recvsize = 50;
+    int recvbuf[recvsize];
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int sendsize = size * recvsize;
+    int *sendbuf = (int *) malloc(sendsize * sizeof(int));
+
+    if (rank == 0) {
+        generate_data(sendbuf, sendsize);
+    }
+
+    MPI_Scatter(sendbuf, recvsize, MPI_INT, recvbuf, recvsize, MPI_INT, 0, MPI_COMM_WORLD);
+
+    print_data(recvbuf, recvsize, rank);
+
     MPI_Finalize();
     return 0;
 }
